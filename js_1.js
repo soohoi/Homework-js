ba4cbaedef2af28ea937546aa603384fb3a4b6b5
soohoi <kiska1770@gmail.com>
2022-05-10 23:55:30 +0300

    fast

--- js_1.js
+++ js_1.js
@@ -53,7 +53,7 @@ if (age_1 < age_2) {
     console.log("You don`t have access cause your age is " + age_1 + " It`s less then")}
     else if (age_1 >= age_2 && age_1 < age_3){
     console.log("Welcome  !");}
-    else if (age_1 > age_3){
+    else if (age_1 >= age_3){
     console.log("Keep calm and look Culture channel");
 }else{
     console.log("Technical work")}
@@ -71,7 +71,7 @@ let checkAge = function(age) {
         console.log("You don`t have access cause your age is " + age + " It`s less then")}
         else if (age >= age_2 && age < age_3){
         console.log("Welcome  !");}
-        else if (age > age_3){
+        else if (age >= age_3){
         console.log("Keep calm and look Culture channel");
     }else{
         console.log("Technical work")}
