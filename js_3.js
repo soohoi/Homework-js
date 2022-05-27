cff5fbaff1954284014587e2ea920e6cd08864ce
soohoi <kiska1770@gmail.com>
2022-05-27 23:45:49 +0300

    create js_3

---
+++ js_3.js
@@ -0,0 +1,30 @@
+let x = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+let X = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+let n = [ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+let sobaka = "@";
+let strk = "aaaaaaa"
+function js(str){
+  str = string(str)
+  if(str.lenght = 4) {
+    alert('Минимум 5 символов в строке')}
+    else{return sj2}}
+function js2(str){
+  if(str.lenght = 64) {
+    alert('Максимум 64 символа в строке')}
+     else{ return sj3}}
+function js3(str){
+  if(!str.includes(x)){
+    alert('В строке должны быть буквы')}
+     else {return sj4}}
+function js4(str){
+  if(!str.includes(X)) {
+      alert('Должна быть хотя бы одна буква в верхнем регистре ')}
+     else {return sj5}}
+function js5(str){
+  if(!str.includes(sobaka)) {
+    alert('Должна быть хотя бы одна @')}
+     else {return sj6}}
+function js6(str){
+  if(str = ''){
+    alert('Строка не должна быть пустой')}
+     else {console.log('Welcome!')}}
